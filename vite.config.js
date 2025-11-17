80284cdad1d1d015f50c31a03e29fc1608e12dba
Ujjansh Sundram <ujjanshsundram2006@gmail.com>
2025-11-17 12:42:03 +0530

    updated files

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: '**/*.{jsx,js}',
+    })
+  ],
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  },
+  root: '.',
+  publicDir: 'public'
+});
